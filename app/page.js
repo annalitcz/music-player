5c6622c82858883e58c8234079e4542d76ae07e5
Anzz <makrufmustofa79@gmail.com>
2024-09-21 15:29:11 +0700

    update lagu

--- app/page.js
+++ app/page.js
@@ -5,35 +5,102 @@ import CombinedPlayer from '../components/CombinedPlayer';
 import musicFiles from '../public/music';
 
 export default function Home() {
-  const playlist = [
+  const calmPlaylist = [
     { title: 'Reckless', src: musicFiles.reckless },
     { title: '8 Letters', src: musicFiles.eightLetters },
     { title: 'We Dont Talk Anymore', src: musicFiles.wedonttalkanymore },
     { title: 'Drunk Text', src: musicFiles.drunktxt },
     { title: 'Hurts So Good', src: musicFiles.hurtssogood },
     { title: 'Shameless', src: musicFiles.shameless },
-    { title: 'People', src: musicFiles.people},
-    { title: 'Mary On A Cross', src: musicFiles.marionacross},
-    { title: 'Rewrite The Stars', src: musicFiles.rewritethestar},
-    { title: 'Fireflies', src: musicFiles.fireflies},
+    { title: 'People', src: musicFiles.people },
+    { title: 'Wide Awake', src: musicFiles.wideawake },
+    { title: 'One Of The Girls', src: musicFiles.oneofthegirls },
+    { title: 'Play Date', src: musicFiles.playdate },
+    { title: 'Mary On A Cross', src: musicFiles.marionacross },
+    { title: 'Run away', src: musicFiles.runeaway },
+    { title: 'Rewrite The Stars', src: musicFiles.rewritethestar },
+    { title: 'Famy', src: musicFiles.famy },
+    { title: 'Snap', src: musicFiles.snap },
+    { title: 'Fireflies', src: musicFiles.fireflies },
   ];
 
+  const djPlaylist = [
+    { title: 'Ya Odna', src: musicFiles.yaodna },
+    { title: 'Ya Odna X The Trum', src: musicFiles.yaodnaxthetrum },
+    { title: 'Come On Make Me Feel Alive', src: musicFiles.comeonmakemefeelalive },
+    { title: 'Goyang Dayung', src: musicFiles.goyangdayung },
+    { title: 'Malam Pagi X Sampai Bawah', src: musicFiles.malampagi },
+    { title: 'Moonlight', src: musicFiles.moonlight },
+    { title: 'Setiap Kali Bertemu', src: musicFiles.setiapkalikitabertemu },
+    { title: 'Sweet Love', src: musicFiles.sweetlove },
+    { title: 'The Drum Breakbeat', src: musicFiles.thedrum },
+    { title: 'Terek Bale', src: musicFiles.terekbale },
+  ];
+
+  const phonkPlaylist = [
+    {title: 'Murder In My Mind', src:musicFiles.murderinmymind},
+    {title: 'Life In Rio', src:musicFiles.lifeinrio},
+    {title: 'WTF 2', src:musicFiles.wtf2},
+    {title: 'Brodyaga Funk', src:musicFiles.brodyagafunk},
+    {title: 'CurseDevil', src:musicFiles.cursedevil},
+    {title: 'Masha UltraFunk', src:musicFiles.mashaultrafunk},
+    {title: 'Sequencia Da', src:musicFiles.sequenciada},
+    {title: 'Sevdaliza', src:musicFiles.sevdaliza},
+    {title: 'Barte Forte E Danca', src:musicFiles.barteforteedanca},
+    {title: 'Lovely Bastard', src:musicFiles.lovelybastard},
+    {title: 'Montagem Coral', src:musicFiles.montagemcoral},
+    {title: 'Orquestra Maldita', src:musicFiles.orquestramaldita}
+  ]
+
+  const popPlaylist = [
+    { title: 'Kings & Queens', src: musicFiles.kingandqueen },
+    { title: 'Sweet But Psycho', src: musicFiles.sweetbutpsycho },
+    { title: 'Symphony', src: musicFiles.symphony },
+    { title: 'Cupid', src: musicFiles.cupid },
+    { title: 'Last Friday Nights', src: musicFiles.lastfridaynight },
+    { title: 'Moonlight SpeedUp', src: musicFiles.moonlightspeedup },
+    { title: 'Hiding In The Blue', src: musicFiles.hidingintheblue },
+    { title: 'Middle Of The Night', src: musicFiles.middleofthenight },
+    { title: 'Im Good (Blue)', src: musicFiles.imgood },
+    { title: 'Remember Our Summer', src: musicFiles.rememberoursummer },
+    { title: 'Royalty', src: musicFiles.royalty },
+    { title: 'Queen Of Disaster', src: musicFiles.queensofdisaster },
+    { title: 'Tatto', src: musicFiles.tatto },
+    { title: 'Stereo Love', src: musicFiles.stereolove },
+    { title: 'Close To The Sun', src: musicFiles.closetothesun },
+    { title: 'Waiting For Love', src: musicFiles.waitingforlove },
+    { title: 'Daylight', src: musicFiles.daylight },
+    { title: 'Derniere Dance', src: musicFiles.dernieredance },
+    { title: 'Love Story', src: musicFiles.indilalovestory },
+    { title: 'Judas', src: musicFiles.judas },
+    { title: 'Side To Side', src: musicFiles.sidetoside },
+    { title: 'Starboy', src: musicFiles.starboy }
+    
+  ];
+
+  const [currentPlaylist, setCurrentPlaylist] = useState(calmPlaylist);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [activePlaylist, setActivePlaylist] = useState('calm'); // Track active playlist
 
-  useEffect(()=>{
-    document.title = 'Music Player'
-  },[])
+  useEffect(() => {
+    document.title = 'Music Player';
+  }, []);
 
-  // Function to play the next song in the playlist
   const handleNextSong = () => {
-    setCurrentSongIndex((prevIndex) => (prevIndex + 1) % playlist.length);
+    setCurrentSongIndex((prevIndex) => (prevIndex + 1) % currentPlaylist.length);
   };
 
-  // Function to play the previous song in the playlist
   const handlePrevSong = () => {
     setCurrentSongIndex((prevIndex) =>
-      prevIndex === 0 ? playlist.length - 1 : prevIndex - 1
+      prevIndex === 0 ? currentPlaylist.length - 1 : prevIndex - 1
     );
+  };
+
+  // Handle changing playlist and active button style
+  const handlePlaylistChange = (playlist, playlistName) => {
+    setCurrentPlaylist(playlist);
+    setActivePlaylist(playlistName); // Set the active playlist
+    setCurrentSongIndex(0); // Reset to the first song of the new playlist
   };
 
   return (
@@ -46,9 +113,45 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center w-full max-w-4xl">
-        {/* CombinedPlayer component */}
+        {/* Playlist Selection Buttons */}
+        <div className="flex space-x-4 mb-5">
+          <button
+            className={`px-4 py-2 rounded-md shadow-md focus:outline-none ${
+              activePlaylist === 'calm' ? 'bg-blue-800 text-white' : 'bg-white text-blue-700 hover:bg-gray-100'
+            }`}
+            onClick={() => handlePlaylistChange(calmPlaylist, 'calm')}
+          >
+            Relax
+          </button>
+          <button
+            className={`px-4 py-2 rounded-md shadow-md focus:outline-none ${
+              activePlaylist === 'dj' ? 'bg-blue-800 text-white' : 'bg-white text-blue-700 hover:bg-gray-100'
+            }`}
+            onClick={() => handlePlaylistChange(djPlaylist, 'dj')}
+          >
+            DJ/Remix
+          </button>
+          <button
+            className={`px-4 py-2 rounded-md shadow-md focus:outline-none ${
+              activePlaylist === 'phonk' ? 'bg-blue-800 text-white' : 'bg-white text-blue-700 hover:bg-gray-100'
+            }`}
+            onClick={() => handlePlaylistChange(phonkPlaylist, 'phonk')}
+          >
+            Phonk
+          </button>
+          <button
+            className={`px-4 py-2 rounded-md shadow-md focus:outline-none ${
+              activePlaylist === 'pop' ? 'bg-blue-800 text-white' : 'bg-white text-blue-700 hover:bg-gray-100'
+            }`}
+            onClick={() => handlePlaylistChange(popPlaylist, 'pop')}
+          >
+            POP
+          </button>
+        </div>
+
+        {/* CombinedPlayer component for playlist */}
         <CombinedPlayer
-          playlist={playlist}
+          playlist={currentPlaylist}
           currentSongIndex={currentSongIndex}
           onSelectSong={setCurrentSongIndex}
           onNextSong={handleNextSong}
--- public/music.js
+++ public/music.js
@@ -1,6 +1,7 @@
 // assets/music/music.js
 
 const musicFiles = {
+  //calm
     reckless: './music/Reckless.mp3',
     eightLetters: '/music/8-letters.mp3',
     wedonttalkanymore: '/music/We Dont Talk Anymore.mp3',
@@ -10,7 +11,61 @@ const musicFiles = {
     shameless: './music/Shameless.mp3',
     marionacross: './music/MARY ON A CROSS-GHOST   Audio Edit - Therplane Musique.m4a',
     rewritethestar: './music/Rewrite The Stars - Anne Marie   James Arthur (sped up) - Sk.m4a',
-    fireflies: './music/Owl City - Fireflies (Speedup) Tiktok Version.mp3'
+    fireflies: './music/Owl City - Fireflies (Speedup) Tiktok Version.mp3',
+    famy: './music/AvaMax - Famy.m4a',
+    playdate: './music/Mellanie Martinez - Play Date Violin Cover.mp3',
+    oneofthegirls: './music/One_Of_The_Girls_sped_up_The_Weeknd_JENNIE_&_Lily_Rose_Depp.mp3',
+    snap: './music/Rosa Linn - Snap.mp3',
+    runeaway: './music/y2mate.com - AURORA  Runaway.mp3',
+    wideawake: './music/y2mate.com - Katy Perry  wide awake sped up.mp3',
+    // dj
+    yaodna: './music/dj/DJ_YA_ODNA_SOUND_THE_KING_PLAT_KT_KANE_SLOWED_+_REVERB_VIRAL_T.m4a',
+    yaodnaxthetrum: './music/dj/DJ_YA_ODNA_X_THE_TRUM_🎧💙_𝙎𝙡𝙤𝙬𝙚𝙙&𝙍𝙚𝙫𝙚𝙧𝙗.mp3',
+    comeonmakemefeelalive: './music/dj/y2mate_com_DJ_COME_ON_MAKE_ME_FEEL_ALIVE_X_DOTA_FULL_BASS_LAGU_DJ.mp3',
+    goyangdayung: './music/dj/y2mate_com_DJ_GOYANG_DAYUNG_BREAKBEAT_MENGKANE_VIRAL_TIKTOK.mp3',
+    malampagi: './music/dj/y2mate_com_DJ_MALAM_PAGI_X_SAMPE_BAWAH_MENGKANE_VIRAL_FYP_TIKTOK.mp3',
+    moonlight: './music/dj/y2mate_com_Dj_Moonlight_Santuy_Viral_Tiktok_Style_Trabas_Yang_Kalian.mp3',
+    setiapkalikitabertemu: './music/dj/y2mate_com_DJ_SETIAP_KALI_KITA_BERTEMU_AKU_LIHAT_KAMU_SENYUM_PADAKU.mp3',
+    sweetlove: './music/dj/y2mate_com_DJ_SWEET_LOVE_REMIX_VIRAL_TIKTOK_TERBARU_YANG_PALING.mp3',
+    thedrum: './music/dj/y2mate_com_DJ_THE_DRUM_BREAKBEAT_VIRAL_TIK_TOK_TERBARU_2024.mp3',
+    terekbale: './music/dj/y2mate_com_SOUND_JJ_DJ_TEREK_BALE_FULL_BASS_SPEED_UP_VIRAL_TIKTOK.mp3',
+    //pop
+    kingandqueen: './music/pop/Ava Max - Kings & Queens.mp3',
+    sweetbutpsycho: './music/pop/Ava Max - Sweet but Psycho [Official Music Video].mp3',
+    thenights: './music/pop/Avicii - The Nights (Lyrics).mp3',
+    symphony: './music/pop/Clean Bandit - Symphony ft. Zara Larsson.mp3',
+    cupid: './music/pop/FIFTY FIFTY - Cupid (Twin Version) sped up.mp3',
+    lastfridaynight: './music/pop/last friday night   TikTok Version   speed up   (lyrics). -.m4a',
+    moonlightspeedup: './music/pop/moonlight (kali uchis) (sped up Version).mp3',
+    hidingintheblue: './music/pop/Nightcore - Hiding In The Blue (Lyrics) - Nightcore Zodiac.m4a',
+    middleofthenight: './music/pop/Nightcore - Middle of the Night (Elley Duhé) - (Lyrics).mp3',
+    imgood: './music/pop/Nightcore → Im Good.mp3',
+    rememberoursummer: './music/pop/Remember Our Summer.m4a',
+    royalty: './music/pop/Royalty - Maestro Chives.m4a',
+    queensofdisaster: './music/pop/SirLofi - Queen Of Disaster.mp3',
+    tatto: './music/pop/Sped Up) - RubyChan s Nightcore.m4a',
+    closetothesun: './music/pop/TheFatRat   Anjulie - Close To The Sun - TheFatRat.m4a',
+    stereolove: './music/pop/tomp3.cc - stereo love  edward maya  vika jigulina sped up.mp3',
+    waitingforlove: './music/pop/Waiting for love.m4a',
+    daylight: './music/pop/y2mate.com - Daylight  Maroon 5 Sped Up.mp3',
+    dernieredance: './music/pop/y2mate.com - Dernière Danse TikTok Sped Up.mp3',
+    indilalovestory: './music/pop/y2mate.com - indila  love story tiktok remix.mp3',
+    judas: './music/pop/y2mate.com - judas  lady gaga full versionedit audio.mp3',
+    sidetoside: './music/pop/y2mate_com_Side_To_Side_Ariana_Grande_ft_Nicki_Minaj_Tiktok_Version.mp3',
+    starboy: './music/pop/y2mate.com - the weeknd  starboy sped up.mp3',
+    // phonk
+    murderinmymind: './music/phonk/KORDHELL - MURDER IN MY MIND - Kordhell.m4a',
+    lifeinrio: './music/phonk/Life in Rio (feat. TOLCHONOV) - NUEKI, Slowboy,   Crazy Mano.m4a',
+    wtf2: './music/phonk/UGOVHB - WTF 2 ( PROD.EF ).mp3',
+    brodyagafunk: './music/phonk/y2mate_com_BRODYAGA_FUNK_SPED_UP_RYOIKI_TENKAI_RAIDEN_SHOGUN_phonk.mp3',
+    cursedevil: './music/phonk/y2mate_com_CURSEDEVIL_DJ_FKU_Skorde_TUCA_DONKA_Brazilian_Phonk.mp3',
+    mashaultrafunk: './music/phonk/y2mate_com_HISTED_TXVSTERPLAYA_MASHA_ULTRAFUNK_Brazilian_Funk_Phonk.mp3',
+    sequenciada: './music/phonk/y2mate_com_SEQUÊNCIA_DA_DZ7_FUNK_BR_FeatMc_Menor_do_Alvorada.mp3',
+    sevdaliza: './music/phonk/y2mate_com_SEVDALIZA_FT_PABLLO_VITTAR_YSEULT_ALIBI_LetraLyrics.mp3',
+    barteforteedanca: './music/phonk/y2mate.com - Bate Forte e Dança BRAZILIAN PHONK.mp3',
+    lovelybastard: './music/phonk/y2mate.com - LOVELY BASTARDS YATASHIGANG ZWE1HVNDXR.mp3',
+    montagemcoral: './music/phonk/y2mate.com - MONTAGEM CORAL.mp3',
+    orquestramaldita: './music/phonk/y2mate.com - ORQUESTRA MALDITA FUNK BR TRASHXRL  MC DELUX.mp3'
   };
   
   export default musicFiles;
